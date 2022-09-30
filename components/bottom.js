073a899a2a74fffaa9577c4f9c1401c47c1805ec
Shrenik MSHA <107319893+mshashrenik@users.noreply.github.com>
2022-09-30 07:51:05 -0400

    switch from flowbite-react to tailwindcss

--- components/bottom.js
+++ components/bottom.js
@@ -1,32 +1,39 @@
 import { Footer } from "flowbite-react";
 
 export default function Bottom() {
-    return (
-        <Footer container={true} rounded={true}>
-            <Footer.LinkGroup>
-                <Footer.Link href="https://github.com/mshashrenik/builders.msha.ml/blob/main/LICENSE">
-                    Copyright (C) 2022 Shrenik S
-                </Footer.Link>
-            </Footer.LinkGroup>
-            <Footer.LinkGroup>
-                <Footer.Link href="/about">
-                    About
-                </Footer.Link>
-                <Footer.Link href="/contact">
-                    Contact
-                </Footer.Link>
-                <Footer.Link href="https://www.christinak12.org/honorsacademy">
-                    MSHA
-                </Footer.Link>
-                <Footer.Link href="https://www.kiwanis.org/">
-                    Kiwanis
-                </Footer.Link>
-                <Footer.Link href="https://forms.gle/xezWd6mZX7fDxvEVA">
-                    Feedback
-                </Footer.Link>
-                <Footer.Link href="https://github.com/mshashrenik/builders.msha.ml/">
-                    Github
-                </Footer.Link>
-            </Footer.LinkGroup>
-        </Footer>)
+  return (
+    <footer class="p-4 bg-white shadow md:flex md:items-center md:justify-between md:p-6 dark:bg-gray-800">
+      <span class="text-sm text-gray-500 sm:text-center dark:text-gray-400">
+        Copyright (C) 2022 Shrenik
+      </span>
+      <ul class="flex flex-wrap items-center mt-3 text-sm text-gray-500 dark:text-gray-400 sm:mt-0">
+        <li>
+          <a href="/about" class="mr-4 hover:underline md:mr-6 ">
+            About
+          </a>
+        </li>
+        <li>
+          <a href="/contact" class="mr-4 hover:underline md:mr-6">
+            Contact
+          </a>
+        </li>
+        <li>
+          <a
+            href="https://www.christinak12.org/honorsacademy"
+            class="mr-4 hover:underline md:mr-6"
+          >
+            MSHA
+          </a>
+        </li>
+        <li>
+          <a
+            href="https://github.com/mshashrenik/builders.msha.ml"
+            class="mr-4 hover:underline md:mr-6"
+          >
+            Github
+          </a>
+        </li>
+      </ul>
+    </footer>
+  );
 }
